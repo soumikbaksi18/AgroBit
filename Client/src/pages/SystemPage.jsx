79d3167c7bc9600128dedf706bb66a18a649ab31
Pranay Goel <goel.2002pranay@gmail.com>
2023-09-09 23:31:36 +0530

    fix: logoutbtn

--- Client/src/pages/SystemPage.jsx
+++ Client/src/pages/SystemPage.jsx
@@ -6,7 +6,7 @@ import Logo from "../components/Logo";
 import locator from "../assets/images/Location.svg";
 import calendar from "../assets/images/Calendar.svg";
 import cloudy from "../assets/images/cloudy.svg";
-
+import {FiLogOut} from 'react-icons/fi';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 import CityForm from "../wcomponents/CityForm";
@@ -286,8 +286,8 @@ const SystemPage = () => {
           <h2 className="flex items-center mr-2 font-semibold text-xl">
             Hello, {currentUser?.displayName ?? currentUser?.email}
           </h2>
-          <button onClick={logout} className="mx-2">
-            Logout
+          <button onClick={logout} className="mx-2" title="Logout">
+            <FiLogOut className="text-primary text-lg"/>
           </button>
           <img
             src={currentUser?.photoURL ?? profile}
