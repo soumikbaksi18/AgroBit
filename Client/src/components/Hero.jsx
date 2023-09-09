c97a8e0a8bd5111fec53f0c98f0b0a789d519e32
soumikbaksi18 <baksisoumik@gmail.com>
2023-09-09 15:16:47 +0530

    Ui update

--- Client/src/components/Hero.jsx
+++ Client/src/components/Hero.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import screens from '../assets/images/screens.png';
 
 const Hero = () => {
   return (
     <div>
         <div className='flex justify-center items-center flex-col'>
-        <div className=" mt-8 text-center text-green-900 text-[56px] font-semibold leading-[84px]">We empower agriculture by helping you make smarter choices</div>
+        <div className=" mt-20 text-center text-green-900 text-7xl font-semibold leading-[84px]">We empower agriculture by helping you <br/> make smarter choices</div>
 
           <p className="mt-4 text-center text-zinc-700 text-lg font-semibold leading-[27px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.</p>
         </div>
 
         <div className='flex justify-center mt-12'>
-          <div className='px-8 py-4 bg-green-900  rounded-lg'><a href="" className='text-white poppins'>Get Started today</a></div>
+          <div className='px-8 py-4 bg-green-900  rounded-lg'><a href="" className='text-white  font-semibold text-lg poppins'>Get Started today</a></div>
+        </div>
+
+        <div className='my-20 flex justify-center'>
+          <img src={screens} alt="" className="" />
         </div>
 
     </div>
--- Client/src/pages/LandingPage.jsx
+++ Client/src/pages/LandingPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
+import Feature from '../components/Feature';
 
 const LandingPage = () => {
   return (
     <>
     <Header/>
-      <Hero/>
+    <Hero/>
+    <Feature/>
     </>
   )
 }
