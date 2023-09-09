b437ad4125483660b6df0cd81bdb2c40ee2c12fd
soumikbaksi18 <baksisoumik@gmail.com>
2023-09-09 15:23:13 +0530

    Footer added

--- Client/src/pages/LandingPage.jsx
+++ Client/src/pages/LandingPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
 import Feature from '../components/Feature';
+import Footer from '../components/Footer';
 
 const LandingPage = () => {
   return (
@@ -9,6 +10,7 @@ const LandingPage = () => {
     <Header/>
     <Hero/>
     <Feature/>
+    <Footer/>
     </>
   )
 }
