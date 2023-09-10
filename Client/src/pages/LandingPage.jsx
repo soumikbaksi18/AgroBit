0d3be378c4188b015fbaad7f01a3bed7492e2246
soumikbaksi18 <baksisoumik@gmail.com>
2023-09-10 15:34:36 +0530

    React Fade Update

--- Client/src/pages/LandingPage.jsx
+++ Client/src/pages/LandingPage.jsx
@@ -4,14 +4,30 @@ import Hero from '../components/Hero';
 import Feature from '../components/Feature';
 import Footer from '../components/Footer';
 import About from '../components/About';
+import { Fade } from "react-awesome-reveal";
+
 
 const LandingPage = () => {
   return (
     <>
     <Header/>
-    <Hero/>
-    <Feature/>
-    <About/>
+
+    <Fade>
+     {""}
+     <Hero/>
+    </Fade>
+    
+    <Fade>
+     {""}
+     <Feature/>
+    </Fade>
+    
+
+    <Fade>
+     {""}
+     <About/>
+    </Fade>
+    
     <Footer/>
     </>
   )
